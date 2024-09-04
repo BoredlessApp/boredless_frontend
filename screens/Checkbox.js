02eb5925ca4ac1161f36ac6ae98dafab320fbfc8
haghighi <jaydon.haghighi@gmail.com>
2024-09-04 15:58:15 -0400

    cleaned up structure of files

--- screens/Checkbox.js
+++ screens/Checkbox.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Image, TouchableOpacity } from 'react-native';
 
-const checkboxImage = require('./assets/checkbox.png');
-const uncheckedImage = require('./assets/unchecked.png');
+const checkboxImage = require('../assets/other_icons/checkbox.png');
+const uncheckedImage = require('../assets/other_icons/unchecked.png');
 
-const circlecheckboxImage = require('./assets/circle_checkbox.png');
-const circleuncheckedImage = require('./assets/circle_unchecked.png');
+const circlecheckboxImage = require('../assets/other_icons/circle_checkbox.png');
+const circleuncheckedImage = require('../assets/other_icons/circle_unchecked.png');
 
 const SquareCheckbox = ({ value, onValueChange }) => {
     return (
